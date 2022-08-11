6ffc18d540d71ba48e1e5b32274a1b07655deec1
Carter Roeser <carter@cdgtech.one>
2022-08-11 15:25:02 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Making New Icons
+
+All icons are made in Adobe Illustrator with a 1000 x 1000 pt artboard. For the box variant icons, use a 150 pt corner radius.
---
+++ dist/techFontColor.js
@@ -0,0 +1,61 @@
+var elasticsearch = document.getElementsByClassName("tf-elasticsearch-clr");
+for (var i = 0; i < elasticsearch.length; i++) {
+    var pt1 = elasticsearch[i].cloneNode(true);
+    pt1.classList.remove("tf-elasticsearch-clr");
+    pt1.classList.add("tf-elasticsearch-pt1");
+    var pt2 = elasticsearch[i].cloneNode(true);
+    pt2.classList.remove("tf-elasticsearch-clr");
+    pt2.classList.add("tf-elasticsearch-pt2");
+    var pt3 = elasticsearch[i].cloneNode(true);
+    pt3.classList.remove("tf-elasticsearch-clr");
+    pt3.classList.add("tf-elasticsearch-pt3");
+    var pt4 = elasticsearch[i].cloneNode(true);
+    pt4.classList.remove("tf-elasticsearch-clr");
+    pt4.classList.add("tf-elasticsearch-pt4");
+    var pt5 = elasticsearch[i].cloneNode(true);
+    pt5.classList.remove("tf-elasticsearch-clr");
+    pt5.classList.add("tf-elasticsearch-pt5");
+    var pt6 = elasticsearch[i].cloneNode(true);
+    pt6.classList.remove("tf-elasticsearch-clr");
+    pt6.classList.add("tf-elasticsearch-pt6");
+
+    elasticsearch[i].parentNode.insertBefore(pt1, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt2, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt3, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt4, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt5, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt6, elasticsearch[i].nextSibling)
+}
+
+var elasticsearch = document.getElementsByClassName("tf-elasticsearch-clr-b");
+for (var i = 0; i < elasticsearch.length; i++) {
+    var pt1 = elasticsearch[i].cloneNode(true);
+    pt1.classList.remove("tf-elasticsearch-clr-b");
+    pt1.classList.add("tf-elasticsearch-pt1");
+    var pt2 = elasticsearch[i].cloneNode(true);
+    pt2.classList.remove("tf-elasticsearch-clr-b");
+    pt2.classList.add("tf-elasticsearch-pt2");
+    var pt3 = elasticsearch[i].cloneNode(true);
+    pt3.classList.remove("tf-elasticsearch-clr-b");
+    pt3.classList.add("tf-elasticsearch-pt3");
+    var pt4 = elasticsearch[i].cloneNode(true);
+    pt4.classList.remove("tf-elasticsearch-clr-b");
+    pt4.classList.add("tf-elasticsearch-pt4");
+    var pt5 = elasticsearch[i].cloneNode(true);
+    pt5.classList.remove("tf-elasticsearch-clr-b");
+    pt5.classList.add("tf-elasticsearch-pt5");
+    var pt6 = elasticsearch[i].cloneNode(true);
+    pt6.classList.remove("tf-elasticsearch-clr-b");
+    pt6.classList.add("tf-elasticsearch-pt6");
+    var pt7 = elasticsearch[i].cloneNode(true);
+    pt7.classList.remove("tf-elasticsearch-clr-b");
+    pt7.classList.add("tf-background");
+
+    elasticsearch[i].parentNode.insertBefore(pt1, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt2, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt3, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt4, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt5, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt6, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt7, elasticsearch[i].nextSibling)
+}

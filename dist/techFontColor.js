1173bf8e1eccb30ffffc58660ba9e4b5a012c88e
Carter Roeser <carter@cdgtech.one>
2022-08-11 17:23:52 -0700

    Add Vue, Fix MultiColor Sizing

--- dist/techFontColor.js
+++ dist/techFontColor.js
@@ -18,6 +18,9 @@ for (var i = 0; i < elasticsearch.length; i++) {
     var pt6 = elasticsearch[i].cloneNode(true);
     pt6.classList.remove("tf-elasticsearch-clr");
     pt6.classList.add("tf-elasticsearch-pt6");
+    var pt7 = elasticsearch[i].cloneNode(true);
+    pt7.classList.remove("tf-elasticsearch-clr");
+    pt7.classList.add("tf-blank");
 
     elasticsearch[i].parentNode.insertBefore(pt1, elasticsearch[i].nextSibling)
     elasticsearch[i].parentNode.insertBefore(pt2, elasticsearch[i].nextSibling)
@@ -25,6 +28,7 @@ for (var i = 0; i < elasticsearch.length; i++) {
     elasticsearch[i].parentNode.insertBefore(pt4, elasticsearch[i].nextSibling)
     elasticsearch[i].parentNode.insertBefore(pt5, elasticsearch[i].nextSibling)
     elasticsearch[i].parentNode.insertBefore(pt6, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt7, elasticsearch[i].nextSibling)
 }
 
 var elasticsearch = document.getElementsByClassName("tf-elasticsearch-clr-b");
@@ -92,6 +96,44 @@ for (var i = 0; i < elasticsearch.length; i++) {
     var pt3 = elasticsearch[i].cloneNode(true);
     pt3.classList.remove("tf-mongodb-clr");
     pt3.classList.add("tf-mongodb-pt3");
+    var pt4 = elasticsearch[i].cloneNode(true);
+    pt4.classList.remove("tf-mongodb-clr");
+    pt4.classList.add("tf-blank");
+
+    elasticsearch[i].parentNode.insertBefore(pt1, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt2, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt3, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt4, elasticsearch[i].nextSibling)
+}
+
+var elasticsearch = document.getElementsByClassName("tf-vue-clr-b");
+for (var i = 0; i < elasticsearch.length; i++) {
+    var pt1 = elasticsearch[i].cloneNode(true);
+    pt1.classList.remove("tf-vue-clr-b");
+    pt1.classList.add("tf-vue-pt1");
+    var pt2 = elasticsearch[i].cloneNode(true);
+    pt2.classList.remove("tf-vue-clr-b");
+    pt2.classList.add("tf-vue-pt2");
+    var pt3 = elasticsearch[i].cloneNode(true);
+    pt3.classList.remove("tf-vue-clr-b");
+    pt3.classList.add("tf-background");
+
+    elasticsearch[i].parentNode.insertBefore(pt1, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt2, elasticsearch[i].nextSibling)
+    elasticsearch[i].parentNode.insertBefore(pt3, elasticsearch[i].nextSibling)
+}
+
+var elasticsearch = document.getElementsByClassName("tf-vue-clr");
+for (var i = 0; i < elasticsearch.length; i++) {
+    var pt1 = elasticsearch[i].cloneNode(true);
+    pt1.classList.remove("tf-vue-clr");
+    pt1.classList.add("tf-vue-pt1");
+    var pt2 = elasticsearch[i].cloneNode(true);
+    pt2.classList.remove("tf-vue-clr");
+    pt2.classList.add("tf-vue-pt2");
+    var pt3 = elasticsearch[i].cloneNode(true);
+    pt3.classList.remove("tf-vue-clr");
+    pt3.classList.add("tf-blank");
 
     elasticsearch[i].parentNode.insertBefore(pt1, elasticsearch[i].nextSibling)
     elasticsearch[i].parentNode.insertBefore(pt2, elasticsearch[i].nextSibling)

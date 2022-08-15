7e63202606ce58b21843764280e2183855adea40
Carter Roeser <carter@cdgtech.one>
2022-08-14 17:05:45 -0700

    Update techFontColor.js

--- dist/techFontColor.js
+++ dist/techFontColor.js
@@ -1,54 +1,55 @@
-function tfLoadMultiColorIcon(icon, parts, box) {
-    var boxString = box ? "-b" : "";
-    var parents = document.getElementsByClassName("tf-" + icon + "-clr" + boxString);
+function tfLoadMultiColorIcon(icon, parts) {
+    var parents = document.getElementsByClassName("tf-" + icon + "-clr");
     for (var i = 0; i < parents.length; i++) {
         if (!parents[i].classList.contains("tf-processed")) {
             var wrapper = document.createElement("div");
             wrapper.classList.add("tf-wrapper");
-            if (box) {
-                var part = parents[i].cloneNode(true);
-                part.classList.remove("tf-" + icon + "-clr" + boxString);
-                part.classList.add("tf-background");
-                wrapper.appendChild(part);
-            } else {
-                var part = parents[i].cloneNode(true);
-                part.classList.remove("tf-" + icon + "-clr" + boxString);
-                part.classList.add("tf-blank");
-                wrapper.appendChild(part);
-            }
+            var part = parents[i].cloneNode(true);
+            part.classList.remove("tf-" + icon + "-clr");
+            part.classList.add("tf-background");
+            wrapper.appendChild(part);
             for (var j = parts; j >= 1; j--) {
                 var part = parents[i].cloneNode(true);
-                part.classList.remove("tf-" + icon + "-clr" + boxString);
+                part.classList.remove("tf-" + icon + "-clr");
                 part.classList.add("tf-" + icon + "-pt" + j);
                 wrapper.appendChild(part);
             }
             parents[i].appendChild(wrapper)
-            parents[i].classList = "tf-" + icon + "-clr" + boxString + " tf-processed";
+            parents[i].classList = "tf-" + icon + "-clr" + " tf-processed";
+        }
+    }
+    var parents = document.getElementsByClassName("tf-" + icon + "-clr-b");
+    for (var i = 0; i < parents.length; i++) {
+        if (!parents[i].classList.contains("tf-processed")) {
+            var wrapper = document.createElement("div");
+            wrapper.classList.add("tf-wrapper");
+            var part = parents[i].cloneNode(true);
+            part.classList.remove("tf-" + icon + "-clr-b");
+            part.classList.add("tf-blank");
+            wrapper.appendChild(part);
+            for (var j = parts; j >= 1; j--) {
+                var part = parents[i].cloneNode(true);
+                part.classList.remove("tf-" + icon + "-clr-b");
+                part.classList.add("tf-" + icon + "-pt" + j);
+                wrapper.appendChild(part);
+            }
+            parents[i].appendChild(wrapper)
+            parents[i].classList = "tf-" + icon + "-clr-b" + " tf-processed";
         }
     }
 }
 
 function tfLoadColors() {
-    tfLoadMultiColorIcon("elasticsearch", 6, false)
-    tfLoadMultiColorIcon("elasticsearch", 6, true)
-    tfLoadMultiColorIcon("mongodb", 3, false)
-    tfLoadMultiColorIcon("mongodb", 3, true)
-    tfLoadMultiColorIcon("vue", 2, false)
-    tfLoadMultiColorIcon("vue", 2, true)
-    tfLoadMultiColorIcon("vite", 2, false)
-    tfLoadMultiColorIcon("vite", 2, true)
-    tfLoadMultiColorIcon("javascript", 2, false)
-    tfLoadMultiColorIcon("javascript", 2, true)
-    tfLoadMultiColorIcon("html", 3, false)
-    tfLoadMultiColorIcon("html", 3, true)
-    tfLoadMultiColorIcon("css", 3, false)
-    tfLoadMultiColorIcon("css", 3, true)
-    tfLoadMultiColorIcon("nuxt", 2, false)
-    tfLoadMultiColorIcon("nuxt", 2, true)
-    tfLoadMultiColorIcon("cloudflare", 2, false)
-    tfLoadMultiColorIcon("cloudflare", 2, true)
-    tfLoadMultiColorIcon("python", 2, false)
-    tfLoadMultiColorIcon("python", 2, true)
+    tfLoadMultiColorIcon("elasticsearch", 6)
+    tfLoadMultiColorIcon("mongodb", 3)
+    tfLoadMultiColorIcon("vue", 2)
+    tfLoadMultiColorIcon("vite", 2)
+    tfLoadMultiColorIcon("javascript", 2)
+    tfLoadMultiColorIcon("html", 3)
+    tfLoadMultiColorIcon("css", 3)
+    tfLoadMultiColorIcon("nuxt", 2)
+    tfLoadMultiColorIcon("cloudflare", 2)
+    tfLoadMultiColorIcon("python", 2)
 }
 
 tfLoadColors();
